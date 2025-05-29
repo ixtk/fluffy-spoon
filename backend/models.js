90ac2090770b3e86937c1129c1a8ca7a540b74d0
ixtk <ikhcamot@gmail.com>
2025-05-29 15:45:25 +0400

    Remove context, implement full google auth flow

--- backend/models.js
+++ backend/models.js
@@ -1,52 +1,12 @@
 import mongoose from "mongoose"
 
-const CartItemSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true
-  },
-  quantity: {
-    type: Number,
+const UserSchema = new mongoose.Schema({
+  providerUserId: {
     required: true,
-    min: 1
+    type: String,
+    unique: true
   }
 })
-
-const UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      minLength: 3,
-      maxLength: 40
-    },
-    email: {
-      type: String,
-      required: true,
-      lowercase: true,
-      unique: true,
-      minLength: 5,
-      maxLength: 45,
-      match: /^\S+@\S+\.\S+$/
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-      required: true
-    },
-    cartItems: {
-      type: [CartItemSchema]
-    }
-  },
-  { timestamps: true }
-)
 
 const ProductSchema = new mongoose.Schema(
   {
